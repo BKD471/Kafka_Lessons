040a2b8f8455288f63f16d559803c365b1bccd6b
BKD471 <bhaskarkumardas9@gmail.com>
2025-06-17 14:48:58 +0530

    Added Validation for Configurations

--- demo/src/main/java/kafka/demo/demo/model/ListenerDto.java
+++ demo/src/main/java/kafka/demo/demo/model/ListenerDto.java
@@ -1,12 +1,15 @@
 package kafka.demo.demo.model;
 
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import org.springframework.validation.annotation.Validated;
 
 @Validated
 public record ListenerDto(
-        @NotNull
+        @NotBlank
+        @Size(min = 3, max = 255, message = " Min topic name length must be 3, max must be within 255")
         String topic,
 
         @NotNull
