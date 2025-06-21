82e0e6be3029d2ae3de70ac1df392bb44cbf6792
BKD471 <bhaskarkumardas9@gmail.com>
2025-06-21 19:59:09 +0530

    fixed imports

--- demo/src/main/java/kafka/demo/demo/controllers/IControllerService.java
+++ demo/src/main/java/kafka/demo/demo/controllers/IControllerService.java
@@ -4,7 +4,6 @@ package kafka.demo.demo.controllers;
 import jakarta.validation.Valid;
 import kafka.demo.demo.model.ListenerDTO;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
